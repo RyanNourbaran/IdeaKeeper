87616af7ff9adfa0effe3971d63d6d1fc5e5feb4
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2018-10-26 03:18:31 -0400

    added stickies

--- src/screens/Stickie.js
+++ src/screens/Stickie.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 
+import "./css/Stickie.css";
+
 export default class Stickie extends Component {
   render() {
     return (
-      <div class="container">
-        <div class="title">
+      <div className="container">
+        <div className="title">
           {this.props.type}
           {this.props.title}
         </div>
-        <div class="note">
-          <input type="text" name="fname" />
+        <div className="note">
+          <input type="textarea" />
         </div>
       </div>
     );
